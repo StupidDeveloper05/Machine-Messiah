6a9988df9e737c1004d9eaa35ee70648e98ef931
StupidDeveloper <StupidDeveloper05@gmail.com>
2023-04-13 04:13:32 +0900

    I create my own OpenAI library for our project

--- Machine-Messiah/main.cpp
+++ Machine-Messiah/main.cpp
@@ -1,31 +1,23 @@
 #include <iostream>
 #include <sstream>
-#include <atlstr.h>
 
-#include <openai.hpp>
+#include "utils.h"
+#include "openai_api.hpp"
 
 std::string reply;
-auto messages = nlohmann::json::array({
-    { {"role", "system"}, {"content", "You are a kind helpful assistant."} }
-});
-
-// ansi->unicode->utf8 인코딩
-std::string ANSI2UTF8(std::string ansi_str)
-{
-    return CW2A(CA2W(ansi_str.c_str()).m_psz, CP_UTF8).m_psz;
-}
-
-// utf8 -> unicode -> ansi 디코딩
-std::string UTF82ANSI(std::string utf8_str)
-{
-    return CW2A(CA2W(utf8_str.c_str(), CP_UTF8).m_psz).m_psz;
-}
+Json::Value messages;
 
 size_t writeFunctionForChat(void* ptr, size_t size, size_t nmemb, std::string* data) 
 {
     std::string json = (char*)ptr;
     std::string str_buf;
     std::istringstream iss(json);
+
+	Json::Value root;
+	Json::CharReaderBuilder builder;
+	Json::CharReader* reader = builder.newCharReader();
+	std::string errors;
+
     if (json.find("data") != std::string::npos)
     {
         while (getline(iss, str_buf, '\n')) {
@@ -33,19 +25,23 @@ size_t writeFunctionForChat(void* ptr, size_t size, size_t nmemb, std::string* d
             {
                 if (str_buf.find("data") != std::string::npos)
                 {
-                    std::string utf8 = nlohmann::json::parse(str_buf.substr(str_buf.find("{"), str_buf.rfind("}") + 1))["choices"][0]["delta"].value("content", "");
-                    std::string ansi = UTF82ANSI(utf8);
-                    reply += ansi;
-                    std::cout << ansi.c_str();
+					std::string generated_response = str_buf.substr(str_buf.find("{"), str_buf.rfind("}") + 1);
+					bool parsing_successful = reader->parse(generated_response.c_str(), generated_response.c_str() + generated_response.size(), &root, &errors);
+					if (parsing_successful)
+					{
+						std::string utf8 = root["choices"][0]["delta"].get("content", "").asCString();
+						std::string ansi = Utf8ToAnsi(utf8);
+						reply += utf8;
+						std::cout << ansi.c_str();
+					}                    
                 }
             }
             catch (std::exception&)
             {
                 std::cout << std::endl;
-                messages.push_back({ {"role", "assistant"}, {"content", ANSI2UTF8(reply).c_str()} });
+                messages.append(OpenAI::CreateMessage("assistant", reply.c_str()));
                 reply = "";
-                openai::set_func(nullptr);
-            }
+			}
         }
     }
     
@@ -53,35 +49,39 @@ size_t writeFunctionForChat(void* ptr, size_t size, size_t nmemb, std::string* d
     return size * nmemb;
 }
 
+
 int main()
 {
-	openai::start(
-        "--- API key ---",
-        "-- organization ID --"
-    );
-    
-    while (true)
-    {
-        std::cout << "User : ";
-        std::string input;
-        getline(std::cin, input);
+	bool successed = OpenAI::Init(
+		"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", 
+		"org-1XK4EGAKbk9RBmHca7zf6HLK"
+	); 
+	if (!successed)
+		return -1;
+	
+	// setting basic system message
+	messages.append(OpenAI::CreateMessage("system", "You are very smart teacher!"));
 
-        // 데이터를 안전하게 utf8로 인코딩함.
-        std::string utf8 = ANSI2UTF8(input);
+	HttpContext->SetWriteFunction(writeFunctionForChat);
 
-        // utf8로 인코딩된 사용자의 입력을 POST할 데이터에 집어넣음.
-        messages.push_back({ {"role", "user"}, {"content", utf8.c_str()} });
+	while (true)
+	{
+		std::cout << "User : ";
+		std::string input;
+		getline(std::cin, input);
 
-        // openai 라이브러리를 수정하여 curl의 write function을 커스터마이징 할 수 있게 함.
-        openai::set_func(writeFunctionForChat);
-        
-        std::cout << "AI : ";
-        // chat gpt에 데이터 POST
-        auto chat = openai::chat().create({
-            {"model", "gpt-3.5-turbo"},
-            {"messages", messages},
-            {"temperature", 0},
-            {"stream", true}
-        });
-    }
+		// create message
+		std::string utf8 = AnsiToUtf8(input);
+		messages.append(OpenAI::CreateMessage("user", utf8.c_str()));
+		
+		// create json body
+		Json::Value json_body;
+		json_body["model"] = "gpt-3.5-turbo";
+		json_body["messages"] = messages;
+		json_body["temperature"] = 0;
+		json_body["stream"] = true;
+
+		// http post request
+		auto result = OpenAI::Create(OpenAI::EndPoint::Chat, json_body);
+	}
 }
